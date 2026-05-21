339a7c4ba100ed493998d78e4f68adfe6faccddd
Tamas Piros <2042718+tpiros@users.noreply.github.com>
2026-05-21 14:42:16 +0200

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig({})
